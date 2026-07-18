3ad9dd47712b9e74e7ebb7dd000604e30b453871
Minkii <xmq13824631287@qq.com>
2026-07-18 09:54:58 +0800

    feat: 完成猜数字1-100游戏

---
+++ W1/guess_number.py
@@ -0,0 +1,12 @@
+# 写一个"猜数字游戏"：随机生成 1-100，循环猜，提示大小
+import random
+num = random.randint(1,100)
+for a in range(1,101):
+    a = int(input("请输入一个1-100的整数"))
+    if a>num :
+        print("太大了")
+    elif a==num:
+        print("猜对了")
+        break
+    if a<num :
+        print("太小了")
