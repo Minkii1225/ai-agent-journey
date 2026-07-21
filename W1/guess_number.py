c66a5f34e47ec5d51831cf7e1257d6bdb3dcca09
Minkii <xmq13824631287@qq.com>
2026-07-21 13:44:54 +0800

    feat: 修改猜数字游戏逻辑，使用循环接收用户输入

--- W1/guess_number.py
+++ W1/guess_number.py
@@ -1,8 +1,8 @@
 # 写一个"猜数字游戏"：随机生成 1-100，循环猜，提示大小
 import random
 num = random.randint(1,100)
-for a in range(1,101):
-    a = int(input("请输入一个1-100的整数"))
+while True:
+    a = int(input("请输入一个1-100的整数:"))
     if a>num :
         print("太大了")
     elif a==num:
