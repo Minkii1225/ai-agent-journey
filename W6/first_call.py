45f3fbedcea9cebea29b2686e39a080d7f766bf0
Minkii <xmq13824631287@qq.com>
2026-09-07 09:27:21 +0800

    feat: AI翻译助手v0

--- W6/first_call.py
+++ W6/first_call.py
@@ -5,7 +5,10 @@ from openai import OpenAI
 
 load_dotenv()
 
-client = OpenAI(
+while True:
+    tone = input("请选择语气（1. 正式 2. 口语 3. 幽默）: ")
+    
+    client = OpenAI(
     api_key=os.environ.get('DEEPSEEK_API_KEY'),
     base_url="https://api.deepseek.com"
 )
